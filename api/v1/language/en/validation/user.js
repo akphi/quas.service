fe011ec2a10f1b1d1c6e3ddffb42cf109e48f941
An Phi <aphi@skidmore.edu>
2017-01-19 13:37:22 -0500

    Complete Validation for Username

--- api/v1/language/en/validation/user.js
+++ api/v1/language/en/validation/user.js
@@ -5,7 +5,9 @@ module.exports = {
   "USERNAME_EMPTY": 'Username is empty, really?',
   "USERNAME_DUPLICATED": 'Username is already taken.',
   "USERNAME_LENGTH": "Username must be between {{minLength}} and {{maxLength}} characters.",
-  "USERNAME_RULE_1": "_ and . are not allowed at either ends",
+  "USERNAME_RULE_1": "A-Za-z0-9_. are allowed",
+  "USERNAME_RULE_2": "_ and . are not allowed to stay near one another",
+  "USERNAME_RULE_3": "_ and . are not allowed at either ends",
   "PASSWORD_EMPTY": 'Password is empty, really?',
 
 }
--- api/v1/validators/user.js
+++ api/v1/validators/user.js
@@ -38,13 +38,15 @@ let validateUsername = (req, result, error, callbackGroup) => {
     if (!validatorHelper.validationEmpty(req, "username", usernameError, errorMessage.USERNAME_EMPTY)) {
         async.series([
             (callback) => {
-                validatorHelper.validateLength(req, "username", usernameError, {
+                validatorHelper.validateLength(error, req, User, "username", usernameError, {
                     min: constants.USERNAME_MIN_LENGTH,
                     max: constants.USERNAME_MAX_LENGTH,
                 }, errorMessage.USERNAME_LENGTH, callback);
             },
-            (callback) => { validatorHelper.validateDuplication(req, User, "username", usernameError, error, errorMessage.USERNAME_DUPLICATED, callback); },
-            (callback) => { validatorHelper.validateMatch(req, "username", usernameError, [new RegExp("^(?![_.])"), new RegExp("[^_.]$")], errorMessage.USERNAME_RULE_1, callback) },
+            (callback) => { validatorHelper.validateDuplication(error, req, User, "username", usernameError, errorMessage.USERNAME_DUPLICATED, callback); },
+            (callback) => { validatorHelper.validateMatch(error, req, User, "username", usernameError, [new RegExp("^(?![_.])"), new RegExp("[^_.]$")], errorMessage.USERNAME_RULE_3, callback) },
+            (callback) => { validatorHelper.validateMatch(error, req, User, "username", usernameError, [new RegExp("^(?!.*[_.]{2})$")], errorMessage.USERNAME_RULE_2, callback) },
+            (callback) => { validatorHelper.validateMatch(error, req, User, "username", usernameError, [new RegExp("^[a-zA-Z0-9._]+$")], errorMessage.USERNAME_RULE_1, callback) },
         ], () => {
             validatorHelper.appendResult(result, "username", usernameError);
             callbackGroup();
